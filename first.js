22f63a9b523ea27ed023252d34768adb3e0ca515
yoanagrigorova <31156123+yoanagrigorova@users.noreply.github.com>
2017-08-19 13:37:49 +0300

    first.js modified

--- first.js
+++ first.js
@@ -1,15 +1,19 @@
 //Задача 10: Въведете число от клавиатурата и определете дали е просто. 
 
-var x = Math.floor(Math.random() * 1000 + 1);
-var isPrime = true;
-console.log(x);
-for (var i = 2; i <= Math.sqrt(x); i++) {
-    if (x % i === 0) {
-        isPrime = false;
+function primeNumber(x) {
+    var isPrime = true;
+    for (var i = 2; i <= Math.sqrt(x); i++) {
+        if (x % i === 0) {
+            isPrime = false;
+        }
+    }
+    if (isPrime) {
+        console.log("Prime number.");
+    } else {
+        console.log('Not a prime number.');
     }
 }
-if (isPrime) {
-    console.log("Prime number.");
-} else {
-    console.log('Not a prime number.');
-}
+
+var x = Math.floor(Math.random() * 100 + 1);
+console.log(x);
+primeNumber(x);
